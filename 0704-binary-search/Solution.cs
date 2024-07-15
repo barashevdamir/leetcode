3716f45f28e06f486a614b0791421307c0c93c19
Barashev Damir <118205240+barashevdamir@users.noreply.github.com>
2024-07-15 11:12:18 +0300

    Runtime: 64 ms

---
+++ 0704-binary-search/Solution.cs
@@ -0,0 +1,25 @@
+public class Solution
+{
+    public int Search(int[] nums, int target)
+    {
+        var leftIndex = 0;
+        var rightIndex = nums.Length - 1;
+        while (leftIndex <= rightIndex)
+        {
+            var middleIndex = (leftIndex + rightIndex) / 2;
+            if (nums[middleIndex] == target)
+            {
+                return middleIndex;
+            }
+            if (nums[middleIndex] < target)
+            {
+                leftIndex = middleIndex + 1;
+            }
+            else
+            {
+                rightIndex = middleIndex - 1;
+            }
+        }
+        return -1;
+    }
+}
