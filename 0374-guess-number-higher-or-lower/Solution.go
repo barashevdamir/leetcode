5a130e70b8034629be84be2419f240b7e7ebf57d
Barashev Damir <118205240+barashevdamir@users.noreply.github.com>
2024-07-26 14:08:42 +0500

    Runtime: 1 ms; Memory: 2.11 MB;

---
+++ 0374-guess-number-higher-or-lower/Solution.go
@@ -0,0 +1,27 @@
+package Leetcode
+
+/**
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * func guess(num int) int;
+ */
+
+func guessNumber(n int) int {
+	var left int = 1
+	var right int = n
+	for left <= right {
+		mid := left + (right-left)/2
+		var pick = guess(mid)
+		if pick == 0 {
+			return mid
+		} else if pick == 1 {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+	return 0
+}
